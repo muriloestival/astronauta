114836828367a7c8fe95bbc9843ddc4b0ec4e177
Murilo <muriloestival@gmail.com>
2019-03-09 20:07:56 -0300

    Versao - Aula 4

---
+++ runtime/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1552169922,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/fundo-sheet0.png",
+		"images/solida.png",
+		"images/player-sheet0.png",
+		"images/moeda-sheet0.png",
+		"images/moedapulo-sheet0.png",
+		"images/moedagravidade-sheet0.png",
+		"images/moedainimiga-sheet0.png",
+		"images/ponte-sheet0.png",
+		"images/inimigo-sheet0.png",
+		"images/porta-sheet0.png",
+		"images/porta-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
